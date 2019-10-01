f438e548a51a9b91cd01bafd88bf9c549b088c80
xolova <xolova-vk@mail.ru>
2019-10-02 01:41:10 +0500

    1

---
+++ vk-apps-currency/precache-manifest.314b871ca2fbb9bab142e5c8f1b78758.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "560427ae647e186968e246789dc558e1",
+    "url": "/vk-apps-currency/index.html"
+  },
+  {
+    "revision": "b129aba8fb870c39016f",
+    "url": "/vk-apps-currency/static/css/2.4e89ce74.chunk.css"
+  },
+  {
+    "revision": "a0bf98811297dd38f068",
+    "url": "/vk-apps-currency/static/css/main.981f7211.chunk.css"
+  },
+  {
+    "revision": "b129aba8fb870c39016f",
+    "url": "/vk-apps-currency/static/js/2.88b71531.chunk.js"
+  },
+  {
+    "revision": "a0bf98811297dd38f068",
+    "url": "/vk-apps-currency/static/js/main.6046d5b8.chunk.js"
+  },
+  {
+    "revision": "10551523682265ac1a86",
+    "url": "/vk-apps-currency/static/js/runtime-main.53f2d8ff.js"
+  },
+  {
+    "revision": "4e1ec8403d903dc514271d7328fbdeb3",
+    "url": "/vk-apps-currency/static/media/persik.4e1ec840.png"
+  }
+]);
