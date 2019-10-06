ca4f9d6946e2ec5b7cacd9e680a653ad87fcf045
xolova <xolova-vk@mail.ru>
2019-10-06 19:13:55 +0500

    Update config.php

--- warlord/bot/config.php
+++ warlord/bot/config.php
@@ -6,7 +6,7 @@
 	$owner_id = $arr[0];
 	$album_id = $arr[1];
 
-	$standalone = 'fb97f583727f7c113ebe2140dfce39ee5819871e6c58fcb9827ccdeb5cf96786ccf5ebfc25d82a5aec9a4';
+	$standalone = 'fc0d29c92ea2743eb77a7d8238f61c00ad9556fb047354022bef7c6d1ecf8a2262d9ef27ee619188a533c';
 	$group_token = 'fc0d29c92ea2743eb77a7d8238f61c00ad9556fb047354022bef7c6d1ecf8a2262d9ef27ee619188a533c';
 	$conf = [
 		'standalone' => $standalone,
