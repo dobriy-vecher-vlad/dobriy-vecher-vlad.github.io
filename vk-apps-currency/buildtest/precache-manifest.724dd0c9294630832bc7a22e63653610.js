248b527ff509408089cbd86979e89968ce53f1b4
xolova <xolova-vk@mail.ru>
2019-10-17 20:57:25 +0500

    123

---
+++ vk-apps-currency/buildtest/precache-manifest.724dd0c9294630832bc7a22e63653610.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9a730fb9e7a258defabb5f4f78ff78bd",
+    "url": "/vk-apps-currency/index.html"
+  },
+  {
+    "revision": "54c8e6455b13f61b3e6c",
+    "url": "/vk-apps-currency/static/css/2.4e89ce74.chunk.css"
+  },
+  {
+    "revision": "e1247e6b03aa9a5f0d62",
+    "url": "/vk-apps-currency/static/css/main.981f7211.chunk.css"
+  },
+  {
+    "revision": "54c8e6455b13f61b3e6c",
+    "url": "/vk-apps-currency/static/js/2.dfbe1c52.chunk.js"
+  },
+  {
+    "revision": "e1247e6b03aa9a5f0d62",
+    "url": "/vk-apps-currency/static/js/main.4de3e8f1.chunk.js"
+  },
+  {
+    "revision": "dfcf6a026301db38190f",
+    "url": "/vk-apps-currency/static/js/runtime-main.e7545e94.js"
+  },
+  {
+    "revision": "4e1ec8403d903dc514271d7328fbdeb3",
+    "url": "/vk-apps-currency/static/media/persik.4e1ec840.png"
+  },
+  {
+    "revision": "dfeeed0fa59fb7ae030afa18ad308e86",
+    "url": "/vk-apps-currency/static/media/ua.dfeeed0f.png"
+  }
+]);
