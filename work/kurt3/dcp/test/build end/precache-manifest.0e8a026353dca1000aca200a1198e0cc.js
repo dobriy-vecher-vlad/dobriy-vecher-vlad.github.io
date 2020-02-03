b82e993e1b02efca70da157b7398c7f9ae66835c
xolova <xolova-vk@mail.ru>
2020-02-03 22:09:38 +0500

    123

---
+++ work/kurt3/dcp/test/build end/precache-manifest.0e8a026353dca1000aca200a1198e0cc.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "16358a8262ab42c70966b81949e2efe5",
+    "url": "/work/kurt3/dcp/test/index.html"
+  },
+  {
+    "revision": "a42ae838a16e87b9aa99",
+    "url": "/work/kurt3/dcp/test/static/css/2.4e89ce74.chunk.css"
+  },
+  {
+    "revision": "60da7c84cc1db97d622e",
+    "url": "/work/kurt3/dcp/test/static/css/main.e029eb92.chunk.css"
+  },
+  {
+    "revision": "a42ae838a16e87b9aa99",
+    "url": "/work/kurt3/dcp/test/static/js/2.cbed5256.chunk.js"
+  },
+  {
+    "revision": "60da7c84cc1db97d622e",
+    "url": "/work/kurt3/dcp/test/static/js/main.0b8a5266.chunk.js"
+  },
+  {
+    "revision": "91fae93f22f94592601b",
+    "url": "/work/kurt3/dcp/test/static/js/runtime-main.76680411.js"
+  },
+  {
+    "revision": "86bd6fc26877a6ad60159966f3f275c2",
+    "url": "/work/kurt3/dcp/test/static/media/icomoon.86bd6fc2.woff"
+  }
+]);
