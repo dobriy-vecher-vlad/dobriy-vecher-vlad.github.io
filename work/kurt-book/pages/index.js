3c0492521fda3c4d5fbb47a0dc82ab3545058f94
xolova <xolova-vk@mail.ru>
2020-04-25 02:02:40 +0500

    book

--- work/kurt-book/pages/index.js
+++ work/kurt-book/pages/index.js
@@ -2,7 +2,7 @@ var data = {
 	'custom': true,
 	'text': `
 		<span class="error errorlogo">Привет!</span>
-		<span class="error errortext">Эта страница — главная.<br>Выбери книгу сверху.</span>
+		<span class="error errortext">Эта страница — главная.<br>Когда-нибудь тут будет очень красивое содержание.<br>А пока выбери книгу сверху.</span>
 	`
 }
 var isScriptLoad = true;
--- work/kurt-book/pages/test-book-1/index.js
+++ work/kurt-book/pages/test-book-1/index.js
@@ -2,7 +2,7 @@ var data = {
 	'custom': true,
 	'text': `
 		<span class="error errorlogo">Привет!</span>
-		<span class="error errortext">Эта страница — пример тестовой книги 1.<br>Выбери тему слева.</span>
+		<span class="error errortext">Эта страница — пример тестовой книги 1.<br>Когда-нибудь тут будет очень красивое содержание.<br>А пока выбери тему слева.</span>
 	`
 }
 var isScriptLoad = true;
