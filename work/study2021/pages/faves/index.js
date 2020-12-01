afb151616af3a8287a862b256cecc6916a186c16
xolova <xolova-vk@mail.ru>
2020-12-01 14:17:43 +0500

    Update index.js

--- work/study2021/pages/faves/index.js
+++ work/study2021/pages/faves/index.js
@@ -3,22 +3,19 @@ var data = {
 	'title': 'Избранные страницы',
 	'description': '',
 	'tag': '',
-	'text': `
-		<div class="title">Избранные страницы</div>
-		<div class="text"></div>
-	`
+	'text': ``
 }
 setTimeout(() => {
 	let favesData = localStorage.getItem('faves') == null ? {} : JSON.parse(localStorage.getItem('faves'));
 	if (Object.keys(favesData).length !== 0) {
-		let faves = '<div class="cards">';
+		let faves = '<div class="title">Избранные страницы</div><div class="text"><div class="cards">';
 		Object.keys(favesData).forEach((item) => {
 			faves += `<div class="card ${item}"><a scale class="text" href="${favesData[item].href}"><b>${favesData[item].tag} ${favesData[item].title}</b><span>${favesData[item].preview}</span></a><div class="buttons" inline><div scale class="button icon round primary s" onclick="delFave('${item}');"><span><i class="far fa-times"></i></span></div></div></div>`;
 		});
-		faves += '</div>';
-		document.querySelector('.text').insertAdjacentHTML('beforeend', faves);
+		faves += '</div></div>';
+		document.querySelector('.middle').insertAdjacentHTML('beforeend', faves);
 	} else {
-		document.querySelector('.text').insertAdjacentHTML('beforeend', `<div class="placeholder" icon="&#xf005">Нет избранных страниц</div>`);
+		document.querySelector('.middle').innerHTML = `<div class="text"><div class="placeholder" icon="&#xf005">Нет избранных страниц</div></div>`;
 	}
 }, 0);
 function delFave(fave) {
@@ -26,7 +23,7 @@ function delFave(fave) {
 	delete faves[fave];
 	document.querySelector(`.${fave}`).remove();
 	if (document.querySelectorAll('.card').length == 0) {
-		document.querySelector('.text').insertAdjacentHTML('beforeend', `<div class="placeholder" icon="&#xf005">Нет избранных страниц</div>`);
+		document.querySelector('.middle').innerHTML = `<div class="text"><div class="placeholder" icon="&#xf005">Нет избранных страниц</div></div>`;
 	}
 	localStorage.setItem('faves', JSON.stringify(faves));
 	changeSettings({type: 'faves'});
